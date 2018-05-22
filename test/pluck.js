2a45bb47dcf0a3306ff816ba48aebccf63d95d63
autocommit <autocommit>
2018-05-22 17:38:21 +0000

    Yet more fixing tests

--- test/pluck.js
+++ test/pluck.js
@@ -8,17 +8,23 @@ require('interleavings').test(function (isAsync) {
       isAsync(function() { cb.apply(null, args) })()
   }
 
-  var done = multicb({ pluck: 1 })
-  async(done(), 5, [null, 1])
-  async(done(), 15, [null, 2])
-  async(done(), 10, [null, 3])
-  done(function(err, results) {
-    console.log('done')
-    t.equal(err, null)
-    t.equal(results[0], 1)
-    t.equal(results[1], 2)
-    t.equal(results[2], 3)
-    isAsync.done()
-  })
+  var mcb = multicb({
+		pluck: 1,
+		done: function(results) {
+			console.log('done')
+			t.equal(results[0], 1)
+			t.equal(results[1], 2)
+			t.equal(results[2], 3)
+			isAsync.done()
+		},
+		error: function(err) {
+			console.error(err);
+			t.fail("There should be no errors.");
+		}
+	})
+  async(mcb(), 5, [null, 1])
+  async(mcb(), 15, [null, 2])
+  async(mcb(), 10, [null, 3])
+	mcb.commit()
 
 })
